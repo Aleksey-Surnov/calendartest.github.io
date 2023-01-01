b36bcb2aa0d3e9e74d11a555defb20734c0cc733
Aleksey-Surnov <55957778+Aleksey-Surnov@users.noreply.github.com>
2023-01-01 15:52:50 +0300

    Update getdatebot.js
    add new func.

--- getdatebot.js
+++ getdatebot.js
@@ -3,11 +3,17 @@ let tg = window.Telegram.WebApp;
 
 tg.expand();
 
+tg.MainButton.textColor = "#FFFFFF";
+tg.MainButton.color = "#2cab37";
+
 let result = {val:""};
 var votedata = document.getElementById("buttonvote");
 
 
-votedata.addEventListener('click', function(){result.val = document.getElementById("calendar").value;})
+votedata.addEventListener('click', function(){
+	result.val = document.getElementById("calendar").value;
+	tg.MainButton.setText("Вы выбрали дату" + result.val);
+	tg.MainButton.show();})
 console.log(result.val);
 
 
