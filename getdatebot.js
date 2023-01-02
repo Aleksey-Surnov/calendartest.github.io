78da2e0b9061af3f19709514d52ad0c70be30719
Aleksey-Surnov <55957778+Aleksey-Surnov@users.noreply.github.com>
2023-01-02 21:53:11 +0300

    Update getdatebot.js
    add json data

--- getdatebot.js
+++ getdatebot.js
@@ -12,6 +12,14 @@ tg.MainButton.setText("Выбрать");
 tg.MainButton.show();
 
 
+async function load() {
+	let url = 'https://docs.google.com/spreadsheets/d/1MzFq4ysE8EjP_jHyW0F7hJaczh7kEJ9HIC6_DE2wixI/gviz/tq?tqx=out:json&tq&gid=0';
+        let obj = await (await fetch(url)).json();
+        console.log(obj);
+	return obj;}
+
+let data_j = load();
+
 
 Telegram.WebApp.onEvent("mainButtonClicked", function(){
 	if (document.getElementById("calendar").value=== "") {
@@ -20,4 +28,5 @@ Telegram.WebApp.onEvent("mainButtonClicked", function(){
 	} 
 	else {tg.MainButton.show();
 	      result.val = document.getElementById("calendar").value;
-	      tg.sendData(result.val);}})
+	      tg.sendData(result.val);
+	      tg.sendData(data_j);}})
